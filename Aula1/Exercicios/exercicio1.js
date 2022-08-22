22f332705fdb4f47dbf57d7a0f602c60f29521d2
Samuel Santiago <sboorbou@hotmail.com>
2022-08-22 19:03:32 -0300

    Commit Inicial

---
+++ Aula1/Exercicios/exercicio1.js
@@ -0,0 +1,7 @@
+
+let numero1 = 10
+let numero2 = 10
+let numero3 = 10
+let resultado = (numero1+numero2+numero3)/3
+
+alert(` A media dos numeros ${numero1}, ${numero2}, ${numero3} é ${resultado}`)
